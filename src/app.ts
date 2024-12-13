867a1a754408e409dbfc7891ab32887b932e6ad7
pedrohsampaioo <pedrohsampaioo@outlook.com>
2024-12-13 11:24:45 -0300

    feat: setup Pino logger

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { notFound, onError } from "stoker/middlewares";
+
+import { logger } from "./middlewares/pino";
 
 const app = new OpenAPIHono();
+
+app.use(logger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.notFound(notFound);
+app.onError(onError);
 
 export default app;
