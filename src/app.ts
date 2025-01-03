9d69d8ed45480f655c70bfa8597afdf7288030fa
pedrohsampaioo <pedrohsampaioo@outlook.com>
2025-01-03 14:47:54 -0300

    feat: export app Hono Type

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,9 @@ const app = createApp();
 const routes = [
   index,
   tasks,
-];
+] as const;
 configureOpenAPI(app);
 routes.forEach(route => app.route("/", route));
+
+export type AppType = typeof routes[number];
 export default app;
